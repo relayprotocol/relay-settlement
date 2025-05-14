78810eb04e9b967889eac50596f5f62513dfb233
Clément Renaud <clement+git@clementrenaud.com>
2025-05-14 21:48:43 +0200

    add arb sepolia support (#6)

--- smart-contracts/hardhat.config.ts
+++ smart-contracts/hardhat.config.ts
@@ -2,8 +2,45 @@ import type { HardhatUserConfig } from 'hardhat/config'
 import '@nomicfoundation/hardhat-toolbox-viem'
 import '@nomicfoundation/hardhat-ignition'
 
+// get pk from shell
+const { DEPLOYER_PRIVATE_KEY } = process.env
+if (!DEPLOYER_PRIVATE_KEY) {
+  console.error(
+    '⚠️ Missing DEPLOYER_PRIVATE_KEY environment variable. Please set one. In the meantime, we will use default settings'
+  )
+} else {
+  console.error(
+    '⚠️ Using account from DEPLOYER_PRIVATE_KEY environment variable.'
+  )
+}
+
+// https://github.com/relayprotocol/relay-vaults/blob/3085d0d1c04f1928f3e4a07e48d1687985570abd/packages/networks/src/networks/testnets/arbitrum-sepolia.ts
+let accounts
+if (DEPLOYER_PRIVATE_KEY) {
+  accounts = [DEPLOYER_PRIVATE_KEY]
+}
+const networks = {
+  'arbitrum-sepolia': {
+    accounts,
+    chainId: 421614,
+    name: 'Arbitrum Sepolia',
+    url: 'https://sepolia-rollup.arbitrum.io/rpc',
+  },
+}
+
+const etherscan = {
+  apiKey: {
+    arbitrumSepolia: 'W5XNFPZS8D6JZ5AXVWD4XCG8B5ZH5JCD4Y',
+  },
+}
+
 const config: HardhatUserConfig = {
+  etherscan,
+  networks,
   solidity: '0.8.28',
+  sourcify: {
+    enabled: true,
+  },
 }
 
 export default config
