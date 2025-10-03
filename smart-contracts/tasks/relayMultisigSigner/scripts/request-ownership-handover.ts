54e2117491032d3d1afb86b766ff30a95e31ee0f
George Roman <30772943+georgeroman@users.noreply.github.com>
2025-10-03 15:07:42 +0200

    chore: request-ownership-handover transactions (#147)

--- smart-contracts/tasks/relayMultisigSigner/scripts/request-ownership-handover.ts
+++ smart-contracts/tasks/relayMultisigSigner/scripts/request-ownership-handover.ts
@@ -1,19 +1,23 @@
 import { createPublicClient, http, parseEther } from 'viem'
-
+import axios from 'axios'
 const main = async () => {
-  const rpcUrls = [
-    'https://rpc.mevblocker.io',
-    'https://mainnet.optimism.io',
-    'https://polygon-rpc.com',
-    'https://api.mainnet.abs.xyz',
-    'https://mainnet.base.org',
-    'https://arb1.arbitrum.io/rpc',
+  const chainIds = [
+    146, 324, 360, 747, 1514, 1868, 2818, 5000, 33139, 55244, 57073, 80094,
+    98866, 534352, 747474, 984122, 7777777,
   ]
 
+  const chains = await axios('https://api.relay.link/chains').then(
+    (r) => r.data.chains
+  )
+
   const txs = await Promise.all(
-    rpcUrls.map(async (rpcUrl) => {
+    chainIds.map(async (chainId) => {
+      const chain = chains.find((c: any) => c.id === chainId)
+      if (chainId === 98866) chain.httpRpcUrl = 'https://rpc.plume.org'
+      if (chainId === 7777777) chain.httpRpcUrl = 'https://rpc.zora.energy'
+
       const rpc = createPublicClient({
-        transport: http(rpcUrl),
+        transport: http(chain.httpRpcUrl),
       })
 
       const fees = await rpc.estimateFeesPerGas()
@@ -22,7 +26,7 @@ const main = async () => {
         amount: '0',
         calldata: '0x25692962',
         from: '0x16c4dEEB433bde1804d8f17cd1Ba3D29a30f9671',
-        to: '0x5CB1De3603A71Ac2f67b12bFbF095013FE4Ac299',
+        to: chain.protocol.v2.depository,
       } as const
 
       const gas = await rpc.estimateGas({
@@ -34,11 +38,14 @@ const main = async () => {
 
       return {
         family: 'ethereum-vm',
-        gas: gas.toString(),
-        maxFeePerGas: fees.maxFeePerGas.toString(),
-        maxPriorityFeePerGas: fees.maxPriorityFeePerGas.toString(),
+        gas: ((gas * 110n) / 100n).toString(),
+        maxFeePerGas: ((fees.maxFeePerGas * 110n) / 100n).toString(),
+        maxPriorityFeePerGas: (
+          (fees.maxPriorityFeePerGas * 110n) /
+          100n
+        ).toString(),
         nonce: await rpc.getTransactionCount({ address: txData.from }),
-        rpc: rpcUrl,
+        rpc: chain.httpRpcUrl,
         ...txData,
       }
     })
