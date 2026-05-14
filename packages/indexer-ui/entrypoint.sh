2ccd473166800e9b166a95f05bab46418b03a24a
txbì <46839250+0xTxbi@users.noreply.github.com>
2026-05-14 15:39:36 +0100

    fix: proxy indexer ui api (#385)

--- packages/indexer-ui/entrypoint.sh
+++ packages/indexer-ui/entrypoint.sh
@@ -1,8 +1,31 @@
 #!/bin/sh
 set -eu
 
-indexer_api_url=$(printf '%s' "${INDEXER_API_URL:-}" | sed 's/[\/&]/\\&/g')
-sed "s/\${INDEXER_API_URL}/$indexer_api_url/g" \
+if [ -z "${INDEXER_API_URL:-}" ]; then
+  echo "INDEXER_API_URL is required"
+  exit 1
+fi
+
+if [ -z "${AUTH_API_KEY:-}" ]; then
+  echo "AUTH_API_KEY is required"
+  exit 1
+fi
+
+indexer_api_url=$(printf '%s' "$INDEXER_API_URL" | sed 's:/*$::')
+case "$indexer_api_url" in
+  http://*/* | https://*/*)
+    echo "INDEXER_API_URL must be an origin without a path"
+    exit 1
+    ;;
+esac
+
+export INDEXER_API_URL="$indexer_api_url"
+
+envsubst '${INDEXER_API_URL} ${AUTH_API_KEY}' \
+  < /etc/nginx/templates/default.conf.template \
+  > /etc/nginx/conf.d/default.conf
+
+sed "s/\${INDEXER_API_URL}//g" \
   /usr/share/nginx/html/config.template.js \
   > /usr/share/nginx/html/config.js
 
